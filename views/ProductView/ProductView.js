92230c50de2a8d47c814afa59b5620885bbc61c1
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-09-22 14:54:59 +0700

    gak nyangka bisa pake use swr

--- views/ProductView/ProductView.js
+++ views/ProductView/ProductView.js
@@ -1,16 +1,25 @@
 'use client'
 import { Container, Grid } from "@mui/material"
 import useSWR from "swr";
+import axios from "axios";
 
-async function fetcher(url) {
-    const res = await fetch(url);
-    const json = await res.json();
-    return json;
-}
+// async function fetcher(url) {
+//     const res = await fetch(url);
+//     const json = await res.json();
+//     return json;
+// }
+const fetcher = (url, token) =>
+    axios
+        .get(url, {
+            headers: {
+                'X-RapidAPI-Key': 'b66eb3153bmsh1d00af693516ac2p106275jsn0b6d5b9e5b20',
+                'X-RapidAPI-Host': 'mobile-phones2.p.rapidapi.com'
+            }
+        })
+        .then((res) => res.data);
 const ProductView = (props) => {
     const { data, error } = useSWR(
-        "https://jsonplaceholder.typicode.com/comments?_start=0&_limit=18",
-        fetcher
+        [`https://mobile-phones2.p.rapidapi.com/brands`], fetcher
     );
     console.log(data);
     return (
